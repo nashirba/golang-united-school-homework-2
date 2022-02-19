c31548e9301c9ea4e3b14fc101f80c2936efe4ed
nashirba <ashirbayev_n@bankrbk.kz>
2022-02-19 09:40:37 +0600

    vars created

--- solution.go
+++ solution.go
@@ -5,13 +5,16 @@ import "math"
 type CustomInt int
 
 func CalcSquare(sideLen float64, sidesNum CustomInt) float64 {
+	SidesTriangle := 3
+	SidesSquare := 4
+	SidesCircle := 0
 	var Square float64
-	switch sidesNum {
-	case 0:
+	switch int(sidesNum) {
+	case SidesCircle:
 		Square = math.Pi *sideLen * sideLen
-	case 3:
+	case SidesTriangle:
 		Square = math.Pow(0.75, 0.5) * sideLen * sideLen
-	case 4:
+	case SidesSquare:
 		Square = sideLen * sideLen
 	}
 	return Square
